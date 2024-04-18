d297af1e6bf1452383fccaabad9658bb0f3cde6c
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-04-18 10:42:34 +0000

    [Add] レシーバのself

--- new.rb
+++ new.rb
@@ -1,4 +1,10 @@
 class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}に曲がります"
   end
@@ -9,7 +15,15 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
+car.move("右", 5)
 
 car = Car.new
 car.run(5)
+
+class Car
+  def self.run(distance)
+  puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
